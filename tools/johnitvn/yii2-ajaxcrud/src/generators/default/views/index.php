d3bf07b89102e1e674944a1ddc5a0d29cc2c9a4b
Alejandro Mendez <mendezalejandro.e@gmail.com>
2018-01-25 22:26:00 -0300

    CRUD completo

---
+++ tools/johnitvn/yii2-ajaxcrud/src/generators/default/views/index.php
@@ -0,0 +1,73 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+use yii\bootstrap\Modal;
+use yii\helpers\Url;
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\crud\Generator */
+
+$urlParams = $generator->generateUrlParams();
+$nameAttribute = $generator->getNameAttribute();
+echo "<?php\n";
+?>
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\bootstrap\Modal;
+use kartik\grid\GridView;
+use johnitvn\ajaxcrud\CrudAsset; 
+use johnitvn\ajaxcrud\BulkButtonWidget;
+
+/* @var $this yii\web\View */
+<?= !empty($generator->searchModelClass) ? "/* @var \$searchModel " . ltrim($generator->searchModelClass, '\\') . " */\n" : '' ?>
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = <?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>;
+$this->params['breadcrumbs'][] = $this->title;
+
+CrudAsset::register($this);
+
+?>
+<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-index">
+    <div id="ajaxCrudDatatable">
+        <?="<?="?>GridView::widget([
+            'id'=>'crud-datatable',
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'pjax'=>true,
+            'columns' => require(__DIR__.'/_columns.php'),
+            'toolbar'=> [
+                ['content'=>
+                    Html::a('<i class="glyphicon glyphicon-plus"></i>', ['create'],
+                    ['role'=>'modal-remote','title'=> 'Crear <?= Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>','class'=>'btn btn-default']).
+                    Html::a('<i class="glyphicon glyphicon-repeat"></i>', [''],
+                    ['data-pjax'=>1, 'class'=>'btn btn-default', 'title'=>'Resetear grilla']).
+                    '{toggleData}'.
+                    '{export}'
+                ],
+            ],
+            'bordered' => true,
+            'hover' => true,
+            'rowOptions' => function ($model, $key, $index, $grid) {
+                return ['rowid' => $key, 'onclick' => '$(this).addClass("success").siblings().removeClass("success");','style' => 'cursor:pointer'];
+            },          
+            'striped' => true,
+            'condensed' => true,
+            'responsive' => true,          
+            'panel' => [
+                'type' => 'primary', 
+                'heading' => '<i class="glyphicon glyphicon-list"></i> <?= Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?> ',
+                'before'=>'<em></em>',
+            ]
+        ])<?="?>\n"?>
+    </div>
+</div>
+<?='<?php Modal::begin([
+    "id"=>"ajaxCrudModal",
+    "footer"=>"",// always need it for jquery plugin
+])?>'."\n"?>
+<?='<?php Modal::end(); ?>'?>
+
